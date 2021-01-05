a7e3fec61d96fbad7f4b88f331e4233837b13257
Zach Heckert <zach@zachheckert.com>
2021-01-05 13:58:21 -0700

    Fixed the context wrapper. You should be able to make stuff work with context now

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
+import { ContextProvider } from './context';
 
 ReactDOM.render(
- 
-    <App />,
-  
+    <ContextProvider>
+      <App />
+    </ContextProvider>,
   document.getElementById('root')
 );
