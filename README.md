76777754b208c44da7a3841c171afe1a5eec2ec4
Zach Heckert <zach@zachheckert.com>
2021-04-08 16:59:31 -0600

    Modified some buttons, tried using an appbar but this will take some work to not be silly. Will do that tomorrow

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+# Weather
+<!-- ![weapon randomizer app](https://github.com/zheckert/weather/blob/main/weatherScreen.png) -->
+
 This is a full-stack app for generating goofy fantasy weapons and saving your favorites. 
 
 Collect 'em all(?)!
+
+View it [HERE](https://legendary-weapon-generator.herokuapp.com).
+
+Built with [React](https://reactjs.org/), [Axios](https://axios-http.com/), and [Material-UI](https://material-ui.com/). 
+
+
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,10 @@
 import React, { useContext } from "react"
 import { UserContext } from "../components/context/UserProvider"
+
 import { Link } from "react-router-dom"
 import { Button } from "@material-ui/core"
+
+import AppBar from "@material-ui/core/AppBar"
 
 export const Navbar = () => {
 
@@ -12,10 +15,7 @@ export const Navbar = () => {
             <Link to="/">Home</Link>
             <Link to="/about">About</Link>
             <Link to="/profile">Profile</Link>
-            <Button onClick={() => logout()}>Logout</Button>
-            <button onClick={() => logout()}>
-                Logout
-            </button>
+            <Button variant="contained" color="primary" onClick={() => logout()}>Logout</Button>
         </div>
     )
 }
